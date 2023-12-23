756f8011451a001f79627c04a3a7b648694ada6e
IT21226632 <it21226632@my.sliit.lk>
2023-12-23 22:31:51 +0530

    order card and fav-order card done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import {Route , Routes , BrowserRouter as Router} from 'react-router-dom'
 import OrderHistory from './pages/order_history/order_history';
 import FavOrderCard from './components/fav_order_card/fav_order_card';
+
 /* Add HomePage element here by importing
   ex- import HomePage from './home/HomePage';
 */
