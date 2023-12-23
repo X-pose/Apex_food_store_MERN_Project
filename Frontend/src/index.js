db3615624c870adc99f786749c094023fe699d60
mihisarajayasinghe <pasanmihisarajayasinghe@gmail.com>
2023-12-23 21:49:54 +0530

    fix

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Homepage from './pages/homePagw/homePage';
 import './Index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Fragment>
     <App />
+    <Homepage/>
   </React.Fragment>
 );
